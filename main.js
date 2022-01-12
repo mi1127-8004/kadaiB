b7407afc480a8abd385e0a0b0427b07dbaccac77
mi1127-8004 <95006288+mi1127-8004@users.noreply.github.com>
2022-01-12 10:02:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'さようなら, ' + storedName;
+  myHeading.innerHTML = '今日は, ' + storedName + 'さん';
 }
 
 myButton.onclick = function() {
